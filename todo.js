2f62b1df29db83906318006695c89f543ec7a9a6
Luis C. Beltran <lcarlosbeltran@gmail.com>
2021-11-27 06:36:06 -0500

    commit from linux notes and todo

---
+++ todo.js
@@ -0,0 +1,10 @@
+const todos = [
+    'Do some Yoga',
+    'Pray to the lord',
+    'Practice more JavaScript',
+    'Study some French',
+    'Go to the movies with my sons'
+]
+
+console.log(`You have ${todos.length} todos.`)
+console.log(todos[0], todos[todos.length - 2])
